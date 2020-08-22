91dcc420b3a4a875392b7affb8372e030f75c199
zhllm <beyourselfinsist@gmail.com>
2020-08-22 22:50:33 +0800

    init

---
+++ CMakeFiles/realTime.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/realTime.dir/main.cpp.o"
+  "CMakeFiles/realTime.dir/server.cpp.o"
+  "realTime"
+  "realTime.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/realTime.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
